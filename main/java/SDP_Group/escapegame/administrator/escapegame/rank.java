e3106113505a7f942b6916038d5bb72bc8afcba2
MichaelZJS <a85670217@163.com>
2016-05-12 12:48:54 +1200

    Add files via upload

---
+++ main/java/SDP_Group/escapegame/administrator/escapegame/rank.java
@@ -0,0 +1,18 @@
+package SDP_Group.escapegame.administrator.escapegame;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by Administrator on 2016/5/11 0011.
+ */
+public class rank extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rank_interface);
+
+    }
+}
---
+++ main/java/SDP_Group/escapegame/administrator/escapegame/staff.java
@@ -0,0 +1,7 @@
+package SDP_Group.escapegame.administrator.escapegame;
+
+/**
+ * Created by Administrator on 2016/5/11 0011.
+ */
+public class staff {
+}
